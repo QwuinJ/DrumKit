fde6d273757fc65ee30089c06678309adf1960da
QwuinJ <jackbelfield8@gmail.com>
2021-11-19 15:29:19 +0000

    changed styling, added switch case

--- app.js
+++ app.js
@@ -10,24 +10,73 @@ const noises = {
   tink: new Audio("sounds/tink.wav"),
 };
 
-document.addEventListener("keypress", (event) => {
-  if (event.key == "a") {
-    noises.clap.play();
-  } else if (event.key == "s") {
-    noises.hiHat.play();
-  } else if (event.key == "d") {
-    noises.kick.play();
-  } else if (event.key == "f") {
-    noises.openHat.play();
-  } else if (event.key == "g") {
-    noises.boom.play();
-  } else if (event.key == "h") {
-    noises.ride.play();
-  } else if (event.key == "j") {
-    noises.snare.play();
-  } else if (event.key == "k") {
-    noises.tom.play();
-  } else if (event.key == "l") {
-    noises.tink.play();
+// change to switch case
+
+document.addEventListener("keypress", function(event) {
+  sound(event.key);
+})
+
+function sound(key){
+  switch(key) {
+    case "a":
+      console.log("pushed");
+      noises.clap.play();
+      break;
+    case "s":
+      noises.hiHat.play();
+      break;
+    case "d":
+      noises.kick.play();
+      break;
+    case "f":
+      noises.openHat.play();
+      break;
+    case "g":
+      noises.boom.play();
+      break;
+    case "h":
+      noises.ride.play();
+      break;
+    case "j":
+      noises.snare.play();
+      break;
+    case "k":
+      noises.tom.play();
+      break;
+    case "l":
+      noises.tink.play();
+      break;
   }
-});
+}
+
+// document.addEventListener("keypress", (event) => {
+//   if (event.key == "a") {
+//     noises.clap.play();
+//   } else if (event.key == "s") {
+//     noises.hiHat.play();
+//   } else if (event.key == "d") {
+//     noises.kick.play();
+//   } else if (event.key == "f") {
+//     noises.openHat.play();
+//   } else if (event.key == "g") {
+//     noises.boom.play();
+//   } else if (event.key == "h") {
+//     noises.ride.play();
+//   } else if (event.key == "j") {
+//     noises.snare.play();
+//   } else if (event.key == "k") {
+//     noises.tom.play();
+//   } else if (event.key == "l") {
+//     noises.tink.play();
+//   }
+// });
+
+// add event listeners for onclick
+
+let allKeys = document.querySelectorAll("button");
+
+for (let i= 0; i < allKeys.length; i++) {
+  allKeys[i].addEventListener("click", function () {
+    console.log("clicked")
+  });
+}
